434c765d0b6d27c123ebc4c5ccf6b99f65fcdfe6
surajrana52 <surajrana52@gmail.com>
2016-09-06 23:39:25 +0530

    added compare page

--- searchresult.php
+++ searchresult.php
@@ -69,3 +69,184 @@
 				<a href="index.html" class="b-breadCumbs__page">Home</a><span class="fa fa-angle-right"></span><a href="" class="b-breadCumbs__page m-active">Search Results</a>
 			</div>
 		</div><!--b-breadCumbs-->
+		
+		
+		<div class="b-infoBar">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 col-xs-12">
+						<div class="b-infoBar__compare wow zoomInUp" data-wow-delay="0.5s">							
+							<a href="compare.html" class="b-infoBar__compare-item"><span class="fa fa-compress"></span>COMPARE VEHICLES:</a>
+						</div>
+					</div>					
+				</div>
+			</div>
+		</div><!--b-infoBar-->
+		
+		<section class="b-items s-shadow">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 col-sm-8 col-xs-12">
+						<div class="b-items__cars">
+							<div class="b-items__cars-one wow zoomInUp" data-wow-delay="0.5s">
+								<div class="b-items__cars-one-img">
+									<img src="" />
+									<a data-toggle="modal" data-target="#myModal" href="#"></a>									
+									<form action="" method="post">
+										<input type="checkbox" name="check1" id='check1'/>
+										<label for="check1" class="b-items__cars-one-img-check"><span class="fa fa-check"></span></label>
+									</form>
+								</div>
+								<div class="b-items__cars-one-info">
+									<header class="b-items__cars-one-info-header s-lineDownLeft">
+										<h2>Car name</h2>
+										<span>Price</span>
+									</header>
+									<p>
+										
+									</p>
+									<div class="b-items__cars-one-info-km">
+										<span class="fa fa-tachometer"></span> 31,730 KM
+									</div>
+									<div class="b-items__cars-one-info-details">
+										<div class="b-featured__item-links">
+											<a href="#">Registered 2015</a>
+											<a href="#">Used</a>
+											<a href="#">8-Speed Automatic</a>
+											<a href="#">Petrol</a>
+										</div>
+										<a href="detail.html" class="btn m-btn">VIEW DETAILS<span class="fa fa-angle-right"></span></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="b-items__pagination wow zoomInUp" data-wow-delay="0.5s">
+							<div class="b-items__pagination-main">
+								<a data-toggle="modal" data-target="#myModal" href="#" class="m-left"><span class="fa fa-angle-left"></span></a>
+								<span class="m-active"><a href="#">1</a></span>
+								<span><a href="#">2</a></span>
+								<span><a href="#">3</a></span>
+								<span><a href="#">4</a></span>
+								<a href="#" class="m-right"><span class="fa fa-angle-right"></span></a>    
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-4 col-xs-12">
+						<aside class="b-items__aside">
+							<h2 class="s-title wow zoomInUp" data-wow-delay="0.5s">REFINE YOUR SEARCH</h2>
+							<div class="b-items__aside-main wow zoomInUp" data-wow-delay="0.5s">
+								<form>
+									<div class="b-items__aside-main-body">
+										<div class="b-items__aside-main-body-item">
+											<label>SELECT A MAKE</label>
+											<div>
+												<select name="select1" class="m-select">
+													<option value="" selected="">Any Make</option>
+												</select>
+												<span class="fa fa-caret-down"></span>
+											</div>
+										</div>
+										<div class="b-items__aside-main-body-item">
+											<label>SELECT A MODEL</label>
+											<div>
+												<select name="select1" class="m-select">
+													<option value="" selected="">Any Make</option>
+												</select>
+												<span class="fa fa-caret-down"></span>
+											</div>
+										</div>
+										<div class="b-items__aside-main-body-item">
+											<label>PRICE RANGE</label>
+											<div class="slider"></div>
+											<input type="hidden" name="min" value="100" class="j-min" />
+											<input type="hidden" name="max" value="1000" class="j-max" />
+										</div>
+										<div class="b-items__aside-main-body-item">
+											<label>VEHICLE TYPE</label>
+											<div>
+												<select name="select1" class="m-select">
+													<option value="" selected="">Any Type</option>
+												</select>
+												<span class="fa fa-caret-down"></span>
+											</div>
+										</div>
+										<div class="b-items__aside-main-body-item">
+											<label>VEHICLE STATUS</label>
+											<div>
+												<select name="select1" class="m-select">
+													<option value="" selected="">Any Status</option>
+												</select>
+												<span class="fa fa-caret-down"></span>
+											</div>
+										</div>
+										<div class="b-items__aside-main-body-item">
+											<label>FUEL TYPE</label>
+											<div>
+												<select name="select1" class="m-select">
+													<option value="" selected="">All Fuel Types</option>
+												</select>
+												<span class="fa fa-caret-down"></span>
+											</div>
+										</div>
+									</div>
+									<footer class="b-items__aside-main-footer">
+										<button type="submit" class="btn m-btn">FILTER VEHICLES<span class="fa fa-angle-right"></span></button><br />
+										<a href="#">RESET ALL FILTERS</a>
+									</footer>
+								</form>
+							</div>							
+						</aside>
+					</div>
+				</div>
+			</div>
+		</section><!--b-items-->
+		
+		
+		<div class="b-features">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9 col-md-offset-3 col-xs-6 col-xs-offset-6">
+						<ul class="b-features__items">
+							<li class="wow zoomInUp" data-wow-delay="0.3s" data-wow-offset="100">Low Prices, No Haggling</li>
+							<li class="wow zoomInUp" data-wow-delay="0.3s" data-wow-offset="100">Largest Car Database</li>
+							<li class="wow zoomInUp" data-wow-delay="0.3s" data-wow-offset="100">EMI Calculator</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div><!--b-features-->
+		
+		<footer class="b-footer">
+			<a id="to-top" href="#this-is-top"><i class="fa fa-chevron-up"></i></a>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="b-footer__company wow fadeInLeft" data-wow-delay="0.3s">
+							<div class="b-nav__logo">
+								<h3><a href="index.html">Easy<span>Cars</span></a></h3>
+								<h2><a href="index.html">Project For SV.CO 2016</a></h2>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-xs-8">
+						<div class="b-footer__content wow fadeInRight" data-wow-delay="0.3s">
+							<div class="b-footer__content-social">
+								<a href="#"><span class="fa fa-facebook-square"></span></a>
+								<a href="#"><span class="fa fa-twitter-square"></span></a>
+								<a href="#"><span class="fa fa-google-plus-square"></span></a>
+								<a href="#"><span class="fa fa-instagram"></span></a>
+								<a href="#"><span class="fa fa-youtube-square"></span></a>								
+							</div>
+							<nav class="b-footer__content-nav">
+								<ul>
+									<li><a href="index.html">Home</a></li>																											
+									<li><a href="about.html">About</a></li>
+									<li><a href="contacts.html">Contact Us</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer><!--b-footer-->
