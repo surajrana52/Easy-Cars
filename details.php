a6706fbe9f1e81b528077676e950e63a4b4dbb59
surajrana52 <surajrana52@gmail.com>
2016-09-06 14:42:44 +0530

    car detail page

---
+++ details.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <title>Easy Cars</title>
+    <link href="assets/css/master.css" rel="stylesheet">
+</head>
+<body>
+<header class="b-topBar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-xs-6">
+                <div class="b-topBar__addr">
+                    <span class="fa fa-map-marker"></span>
+                    JP Nagar 5th phase, Bangalore
+                </div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="b-topBar__tel">
+                    <span class="fa fa-phone"></span>
+                    1800-111-000
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <nav class="b-topBar__nav">
+                    <ul>
+                        <li><a href="#">Register</a></li>
+                        <li><a href="#">Sign in</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header><!--b-topBar-->
+
+<nav class="b-nav">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 col-xs-4">
+                <div class="b-nav__logo wow slideInLeft">
+                    <h3><a href="index.html">Easy <span>Cars</span></a></h3>
+                    <h2><a href="index.html">Project For SV.CO 2016</a></h2>
+                </div>
+            </div>
+            <div class="col-sm-9 col-xs-8">
+                <div class="b-nav__list wow slideInRight">
+                    <div class="collapse navbar-collapse navbar-main-slide" id="nav">
+                        <ul class="navbar-nav-menu">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="services.html">Services</a></li>
+                            <li><a href="contacts.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav><!--b-nav-->
