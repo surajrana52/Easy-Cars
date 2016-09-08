6cd2d1405ec3a2d1b4a5f13dd7ff3dab4766a2c4
surajrana52 <surajrana52@gmail.com>
2016-09-08 14:06:21 +0530

    database connect

--- php/database.php
+++ php/database.php
@@ -1,3 +1,13 @@
 <?php
+$host="localhost";
+$username="root";
+$userpass="";
+$dbname="sv_co";
 
+try{
+    $DB_con = new PDO("mysql:host={$host};dbname={$dbname}",$username,$userpass);
+    $DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
 ?>
