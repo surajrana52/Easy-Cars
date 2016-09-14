fdda051706a0ee8cdb78a5ea0cc8f3229fe565a9
surajrana52 <surajrana52@gmail.com>
2016-09-14 18:01:09 +0530

    Fovorite page added & login class created

---
+++ favorite.php
@@ -0,0 +1,146 @@
+<html>
+<head>
+    <title>Favorite | Easy Cars</title>
+    <link href="assets/css/master.css" rel="stylesheet">
+</head>
+<body>
+<header class="b-topBar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-xs-6">
+                <div class="b-topBar__addr">
+                    <span class="fa fa-map-marker"></span>
+                    JP Nagar 5th phase, Bangalore
+                </div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="b-topBar__tel">
+                    <span class="fa fa-phone"></span>
+                    1800-111-000
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <nav class="b-topBar__nav">
+                    <ul>
+                        <li><a href="#">Register</a></li>
+                        <li><a href="#">Sign in</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header><!--b-topBar-->
+
+		<section class="b-pageHeader">
+			<div class="container">
+				<h1 class="wow zoomInLeft" data-wow-delay="0.5s">Auto Listings</h1>
+				<div class="b-pageHeader__search wow zoomInRight" data-wow-delay="0.5s">
+					<h3>Your favorite Cars</h3>
+				</div>
+			</div>
+		</section><!--b-pageHeader-->
+
+		<div class="b-breadCumbs s-shadow">
+			<div class="container wow zoomInUp" data-wow-delay="0.5s">
+				<a href="index.html" class="b-breadCumbs__page">Home</a><span class="fa fa-angle-right"></span><a href="javascript:void(0)" class="b-breadCumbs__page m-active">Favorite</a>
+			</div>
+		</div><!--b-breadCumbs-->
+
+
+		<div class="b-items">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-sm-8 col-xs-12">
+						<div class="row">
+							<div class="col-lg-4 col-md-6 col-xs-12">
+								<div class="b-items__cell wow zoomInUp" data-wow-delay="0.5s">
+									<div class="b-items__cars-one-img">
+										<img class='img-responsive' src="" alt=''/>
+										<form action="" method="post">
+											<input type="checkbox" name="check1" id='check1'/>
+											<label for="check1" class="b-items__cars-one-img-check"><span class="fa fa-check"></span></label>
+										</form>
+									</div>
+									<div class="b-items__cell-info">
+										<div class="s-lineDownLeft b-items__cell-info-title">
+											<h2 class="">Chevrolet Silverado 1500</h2>
+										</div>
+										<p>In a pickup market gone fancy, the Silverado sticks to its basic-truck recipe. The steering is accurate, and the ...</p>
+										<div class="row m-smallPadding">
+											<div class="col-xs-6">
+												<ul>
+													<li>Registered 2015</li>
+													<li>Used</li>
+													<li>8-Speed Automatic </li>
+													<li>Petrol</li>
+												</ul>
+												<h5 class="b-items__cell-info-price">$29,415</h5>
+											</div>
+											<div class="col-xs-6">
+												<div class="b-items__cell-info-km">
+													<span class="fa fa-tachometer"></span>
+													<p>31,730 KM</p>
+												</div>
+												<a href="" class="btn m-btn">VIEW DETAILS<span class="fa fa-angle-right"></span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>							
+						</div>						
+					</div>					
+				</div>
+			</div>
+		</div><!--b-items-->
+
+
+<div class="b-features">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9 col-md-offset-3 col-xs-6 col-xs-offset-6">
+						<ul class="b-features__items">
+							<li class="wow zoomInUp" data-wow-delay="0.3s" data-wow-offset="100">Low Prices, No Haggling</li>
+							<li class="wow zoomInUp" data-wow-delay="0.3s" data-wow-offset="100">Largest Car Database</li>
+							<li class="wow zoomInUp" data-wow-delay="0.3s" data-wow-offset="100">EMI Calculator</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div><!--b-features-->
+		
+		<footer class="b-footer">
+			<a id="to-top" href="#this-is-top"><i class="fa fa-chevron-up"></i></a>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="b-footer__company wow fadeInLeft" data-wow-delay="0.3s">
+							<div class="b-nav__logo">
+								<h3><a href="index.html">Easy<span>Cars</span></a></h3>
+								<h2><a href="index.html">Project For SV.CO 2016</a></h2>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-xs-8">
+						<div class="b-footer__content wow fadeInRight" data-wow-delay="0.3s">
+							<div class="b-footer__content-social">
+								<a href="#"><span class="fa fa-facebook-square"></span></a>
+								<a href="#"><span class="fa fa-twitter-square"></span></a>
+								<a href="#"><span class="fa fa-google-plus-square"></span></a>
+								<a href="#"><span class="fa fa-instagram"></span></a>
+								<a href="#"><span class="fa fa-youtube-square"></span></a>								
+							</div>
+							<nav class="b-footer__content-nav">
+								<ul>
+									<li><a href="index.html">Home</a></li>																											
+									<li><a href="about.html">About</a></li>
+									<li><a href="contacts.html">Contact Us</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer><!--b-footer-->
+		</body>
+		</html>
---
+++ php/classes/login.class.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/../database.php';
+
+class login{
+
+    private $db;
+
+    public function register($name,$email,$pass){
+
+        
+
+    }
+
+    public function login($email,$pass){
+
+    }
+
+}
+
+$login = new login($DB_con);
